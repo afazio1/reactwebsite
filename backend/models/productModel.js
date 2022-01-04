b6c990dfa7c55724860c4fb8d3bc1b0760ff3457
afazio1 <alexa.fazio04@gmail.com>
2022-01-04 13:54:23 -0500

    🥽

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -68,6 +68,6 @@ const productSchema = mongoose.Schema({
     timestamps: true
 })
 
-const Product = mongoose.model('Product', userSchema)
+const Product = mongoose.model('Product', productSchema)
 
 export default Product
